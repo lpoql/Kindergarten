439c5a32e8a179b80ef5347abcd119e7364c6559
yaqf <yeovng@gmail.com>
2023-01-06 18:19:43 +0800

    feat: add prettier

--- src/algorithms/sorts/quick-sort/quicksort.ts
+++ src/algorithms/sorts/quick-sort/quicksort.ts
@@ -1,84 +1,84 @@
 /*
-* Quicksort is a divide and conquer algorithm.
-* It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays,
-* according to whether they are less than or greater than the pivot.
-* The sub-arrays are then sorted recursively.
-* This can be done in-place, requiring small additional amounts of memory to perform the sorting.
-* */
+ * Quicksort is a divide and conquer algorithm.
+ * It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays,
+ * according to whether they are less than or greater than the pivot.
+ * The sub-arrays are then sorted recursively.
+ * This can be done in-place, requiring small additional amounts of memory to perform the sorting.
+ * */
 
 /*
-* [1, 2, 6, 8, 4, 3, 7, 5, 3, 9]
-* params: array - array to be sorted
-*         left - left index of the array
-*         right - right index of the array
-* */
+ * [1, 2, 6, 8, 4, 3, 7, 5, 3, 9]
+ * params: array - array to be sorted
+ *         left - left index of the array
+ *         right - right index of the array
+ * */
 
 function swap(arr: Array<number>, i: number, j: number) {
-    return [arr[i], arr[j]] = [arr[j], arr[i]];
+  return ([arr[i], arr[j]] = [arr[j], arr[i]]);
 }
 
 export function Partition(arr: Array<number>, left: number, right: number) {
-    let pivot = arr[Math.floor((right + left) / 2)];
+  let pivot = arr[Math.floor((right + left) / 2)];
 
-    while (left <= right) {
-        while (arr[left] < pivot) {
-            left++;
-        }
-        while (arr[right] > pivot) {
-            right--;
-        }
-        // while arr[left] > pivot and arr[right] < pivot
-        if (left <= right) {
-            swap(arr, left, right);
-            left++;
-            right--;
-        }
+  while (left <= right) {
+    while (arr[left] < pivot) {
+      left++;
     }
-    return left; // return the index of the pivot
+    while (arr[right] > pivot) {
+      right--;
+    }
+    // while arr[left] > pivot and arr[right] < pivot
+    if (left <= right) {
+      swap(arr, left, right);
+      left++;
+      right--;
+    }
+  }
+  return left; // return the index of the pivot
 }
 
 export function Quicksort(arr: Array<number>, left: number, right: number) {
-    let pivotIndex;
-    if (arr.length > 1) {
-        pivotIndex = Partition(arr, left, right);
-        // Divide and conquer
-        if (left < pivotIndex - 1) {
-            Quicksort(arr, left, pivotIndex - 1);
-        }
-        if (pivotIndex < right) {
-            Quicksort(arr, pivotIndex, right);
-        }
+  let pivotIndex;
+  if (arr.length > 1) {
+    pivotIndex = Partition(arr, left, right);
+    // Divide and conquer
+    if (left < pivotIndex - 1) {
+      Quicksort(arr, left, pivotIndex - 1);
     }
-    return arr;
+    if (pivotIndex < right) {
+      Quicksort(arr, pivotIndex, right);
+    }
+  }
+  return arr;
 }
 
 /*
-*
-* Refer https://zh.wikipedia.org/zh-cn/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F
-*       https://en.wikipedia.org/wiki/Quicksort
-*/
+ *
+ * Refer https://zh.wikipedia.org/zh-cn/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F
+ *       https://en.wikipedia.org/wiki/Quicksort
+ */
 
 export function PivotIndex(arr: Array<number>, left: number, right: number, pivot: number): number {
-    let pivotVal = arr[pivot]
-    swap(arr, arr[pivot], arr[right])
+  let pivotVal = arr[pivot];
+  swap(arr, arr[pivot], arr[right]);
 
-    let storeIndex = left
-    for (let i = left; i < right; i++) {
-        if (arr[i] < pivotVal) {
-            swap(arr, storeIndex, i)
-            storeIndex += 1
-        }
+  let storeIndex = left;
+  for (let i = left; i < right; i++) {
+    if (arr[i] < pivotVal) {
+      swap(arr, storeIndex, i);
+      storeIndex += 1;
     }
-    swap(arr, right, storeIndex)
-    return storeIndex
+  }
+  swap(arr, right, storeIndex);
+  return storeIndex;
 }
 
 export function Quicksorting(arr: Array<number>, left: number, right: number) {
-    let pivotIndex;
-    if (right > left) {
-        pivotIndex = PivotIndex(arr, left, right, right);
-        Quicksorting(arr, left, pivotIndex - 1);
-        Quicksorting(arr, pivotIndex + 1, right);
-    }
-    return arr;
+  let pivotIndex;
+  if (right > left) {
+    pivotIndex = PivotIndex(arr, left, right, right);
+    Quicksorting(arr, left, pivotIndex - 1);
+    Quicksorting(arr, pivotIndex + 1, right);
+  }
+  return arr;
 }
--- src/data-structures/stack/stack.ts
+++ src/data-structures/stack/stack.ts
@@ -1,40 +1,40 @@
 /*
-* LIFO(last in first out)
-* S: means stack
-* Push: to add items to the stack
-* Pop: returns the last added item and deletes it from the stack.
-* Size: return the stack length
-* */
+ * LIFO(last in first out)
+ * S: means stack
+ * Push: to add items to the stack
+ * Pop: returns the last added item and deletes it from the stack.
+ * Size: return the stack length
+ * */
 
 interface stackProps<T> {
-    pop(): T[] | undefined
+  pop(): T[] | undefined;
 
-    push(element: T): void
+  push(element: T): void;
 
-    size(): number
+  size(): number;
 }
 
 export default class StackStruct<T> implements stackProps<T> {
-    private S: T[] = [] // init stack
+  private S: T[] = []; // init stack
 
-    constructor() {}
+  constructor() {}
 
-    push(ele: T) {
-        if(this.S.length === Infinity){
-            return Error('Stack has reached maximum capacity')
-        }
-        this.S.push(ele)
-        return this.S
+  push(ele: T) {
+    if (this.S.length === Infinity) {
+      return Error('Stack has reached maximum capacity');
     }
+    this.S.push(ele);
+    return this.S;
+  }
 
-    pop(): T[] | undefined {
-        if(this.S.length!==0){
-            this.S.pop()
-        }
-        return this.S;
+  pop(): T[] | undefined {
+    if (this.S.length !== 0) {
+      this.S.pop();
     }
+    return this.S;
+  }
 
-    size(): number {
-        return this.S.length;
-    }
+  size(): number {
+    return this.S.length;
+  }
 }
