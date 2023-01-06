75139d28c5b1033048f665d201e10517392f9f44
yaqf <yeovng@gmail.com>
2023-01-06 17:53:36 +0800

    test(sorts/quick-sort): add test for quick sort

---
+++ src/algorithms/sorts/quick-sort/quicksort.test.ts
@@ -0,0 +1,15 @@
+import {Quicksorting} from "./quicksort";
+
+describe("Quick Sort", () => {
+    it("should return the correct value for average case", () => {
+        expect(Quicksorting([1, 4, 2, 5, 9, 6, 3, 8, 10, 7],0,9)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+
+    it("should return the correct value for worst case", () => {
+        expect(Quicksorting([10, 9, 8, 7, 6, 5, 4, 3, 2, 1],0,9)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+
+    it("should return the correct value for best case", () => {
+        expect(Quicksorting([1, 4, 2, 9, 5, 7, 3, 8, 10, 6],0,9)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+});
