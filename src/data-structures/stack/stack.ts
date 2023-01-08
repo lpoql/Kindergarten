feb3457154065214b8b94d458f0ffd5d1570417a
Yaqf <q@x2oe.com>
2023-01-08 12:21:43 +0800

    fix(data-structures/stack): correct code

--- src/data-structures/stack/stack.ts
+++ src/data-structures/stack/stack.ts
@@ -2,39 +2,40 @@
  * LIFO(last in first out)
  * S: means stack
  * Push: to add items to the stack
- * Pop: returns the last added item and deletes it from the stack.
+ * Pop: deletes it from the stack and return.
  * Size: return the stack length
  * */
 
 interface stackProps<T> {
-  pop(): T[] | undefined;
+    pop(): T[] | undefined;
 
-  push(element: T): void;
+    push(element: T): void;
 
-  size(): number;
+    size(): number;
 }
 
 export default class StackStruct<T> implements stackProps<T> {
-  private S: T[] = []; // init stack
+    private readonly S: T[] = []; // init stack
 
-  constructor() {}
-
-  push(ele: T) {
-    if (this.S.length === Infinity) {
-      return Error('Stack has reached maximum capacity');
+    constructor(stack?: T[]) {
+        if (stack && stack.length > 0) {
+            this.S = stack
+        }
     }
-    this.S.push(ele);
-    return this.S;
-  }
 
-  pop(): T[] | undefined {
-    if (this.S.length !== 0) {
-      this.S.pop();
+    push(ele: T) {
+        this.S.push(ele);
+        return this.S;
     }
-    return this.S;
-  }
 
-  size(): number {
-    return this.S.length;
-  }
+    pop(): T[] | undefined {
+        if (this.S.length !== 0) {
+            this.S.pop();
+        }
+        return this.S;
+    }
+
+    size(): number {
+        return this.S.length;
+    }
 }
