3ec12ca833209f2d476d82545a73ee287e1e7fea
Yaqf <q@x2oe.com>
2023-02-20 21:38:33 +0800

    fix: test

--- src/algorithms/practice/poker.test.ts
+++ src/algorithms/practice/poker.test.ts
@@ -9,11 +9,15 @@ describe('', () => {
     expect(bestHand([6, 6, 2, 3, 5], ['d', 'a', 'a', 'b', 'c'])).toBe('Pair');
   });
 
-  it('Three of Kind', function () {
-    expect(bestHand([4, 4, 2, 4, 4], ['d', 'a', 'a', 'b', 'c'])).toBe('Three of Kind');
+  it('Three of a Kind', function () {
+    expect(bestHand([4, 4, 2, 4, 4], ['d', 'a', 'a', 'b', 'c'])).toBe('Three of a Kind');
   });
 
   it('High Card', function () {
     expect(bestHand([6, 4, 2, 3, 5], ['d', 'a', 'a', 'b', 'c'])).toBe('High Card');
+  });
+
+  it('Three of a Kind and pair', function () {
+    expect(bestHand([1,1,1,2,2], ["a","b","c","a","d"])).toBe('Three of a Kind');
   });
 });
