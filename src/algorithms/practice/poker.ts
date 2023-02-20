ce8e5eaa14368d983bfe903edb0bec97c633fc1e
yaqf <yeovng@gmail.com>
2023-02-20 18:41:44 +0800

    fmt

--- src/algorithms/practice/poker.ts
+++ src/algorithms/practice/poker.ts
@@ -1,41 +1,41 @@
 /*
-*
-*
-*/
+ *
+ *
+ */
 
 function hashSuits(suits: string[]): number {
-    let list = new Set()
-    for (let i of suits) {
-        list.add(i)
-    }
-    return list.size
+  let list = new Set();
+  for (let i of suits) {
+    list.add(i);
+  }
+  return list.size;
 }
 
 export function bestHand(ranks: number[], suits: string[]): string {
-    let handType = 0
+  let handType = 0;
 
-    if (hashSuits(suits) === 1) {
-        return "Flush" // check Flush
-    }
+  if (hashSuits(suits) === 1) {
+    return 'Flush'; // check Flush
+  }
 
-    let ranksList = new Map<string, number>()
-    for (let i of ranks) {
-        ranksList.set(String(i), (ranksList.get(String(i)) || 0) + 1) // count the single element
-    }
+  let ranksList = new Map<string, number>();
+  for (let i of ranks) {
+    ranksList.set(String(i), (ranksList.get(String(i)) || 0) + 1); // count the single element
+  }
 
-    handType = ranksList.size // check High Card
+  handType = ranksList.size; // check High Card
 
-    ranksList.forEach((value) => {
-        if (value === 2) handType = value // check pair
-        if (value > 2) handType = value
-    })
+  ranksList.forEach(value => {
+    if (value === 2) handType = value; // check pair
+    if (value > 2) handType = value;
+  });
 
-    switch (handType) {
-        case 2:
-            return "Pair"
-        case 5:
-            return "High Card"
-        default:
-            return "Three of a Kind"
-    }
+  switch (handType) {
+    case 2:
+      return 'Pair';
+    case 5:
+      return 'High Card';
+    default:
+      return 'Three of a Kind';
+  }
 }
