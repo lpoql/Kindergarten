1f52792f5c666ff2e7954a1d6f85e1883a0e2086
Yaqf <q@x2oe.com>
2023-02-26 23:06:53 +0800

    feat: add two numbers in linked list

---
+++ src/algorithms/practice/list_sum.ts
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+interface ListNode<T> {
+    val: T
+    next: ListNode<T> | null
+}
+
+export function ArrToList<T>(arr: T[]): ListNode<T> | null {
+    if (!arr.length) return null
+    const node: ListNode<T> = {val: arr[0], next: null}
+    let next = node
+    for (let i = 1; i < arr.length; i++) {
+        next.next = {val: arr[i], next: null}
+        next = next.next
+    }
+    return node
+}
+
+export function addTwoNumbers(l1: ListNode<number> | null, l2: ListNode<number> | null): ListNode<number> | null {
+    let carry: number = 0
+    const Node: ListNode<number> | null = {val: 0, next: null}
+    let node = Node
+
+    while (l1 !== null || l2 !== null) {
+        let sum = (l1?.val || 0) + (l2?.val || 0) + carry
+        carry = Math.floor(sum / 10)
+        sum = sum % 10
+
+        node.next = {val: sum, next: null}
+        node = node.next
+
+        if (l1 !== null) l1 = l1.next
+        if (l2 !== null) l2 = l2.next
+    }
+    return Node.next
+}
+
+export class LinkTable<T> {
+    readonly #node: T // current element
+    #next: undefined | LinkTable<T> // next node
+    #len: number = 0 // list length
+
+    constructor(node: T) {
+        this.#node = node
+    }
+
+    add = (node: T): LinkTable<T> => {
+        this.#next = new LinkTable<T>(node)
+        this.#len++
+        console.log(this.#node, this.next())
+        return this.#next
+    }
+    value = () => this.#node
+    next = () => {
+        if (this.#next) return this.value()
+    }
+}
---
+++ src/algorithms/practice/test/list_sum.test.ts
@@ -0,0 +1,34 @@
+import {addTwoNumbers, ArrToList, LinkTable} from "../list_sum";
+
+describe('arr to list', () => {
+    it('test array to list', function () {
+        expect(ArrToList([7, 0, 8])).toEqual({val: 7, next: {val: 0, next: {val: 8, next: null}}})
+    });
+
+    it('Addition of two numbers in linked list form', function () {
+        expect(addTwoNumbers(ArrToList([2, 4, 3]), ArrToList([5, 6, 4]))).toEqual({
+            val: 7,
+            next: {val: 0, next: {val: 8, next: null}}
+        })
+    });
+
+    it('Boundary case of converting an array to a linked list', () => {
+        expect(ArrToList([])).toBeNull()
+    })
+
+    // it('Linked list-related operations implemented by the test class', () => {
+    //     function linkAdd<T>(arr: T[]) {
+    //         if (!arr.length) return null
+    //         const node = new LinkTable<T>(arr[0]);
+    //         // console.log(node,'node')
+    //         arr.splice(1).forEach(val => {
+    //             console.log(val,'-')
+    //             node.add(val)
+    //         })
+    //         return node
+    //     }
+    //
+    //     // expect(linkAdd([8,0,7])).toEqual({val: 8, next: {val: 0, next: {val: 7, next: null}}})
+    //     linkAdd([8,0,7])
+    // })
+})
