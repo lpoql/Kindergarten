8d726f8b5c67fb5203c91bbd784a7b7ebf30cafe
Yaqf <q@x2oe.com>
2023-02-26 23:07:48 +0800

    fmt

--- src/algorithms/practice/list_sum.ts
+++ src/algorithms/practice/list_sum.ts
@@ -10,57 +10,57 @@
  * }
  */
 interface ListNode<T> {
-    val: T
-    next: ListNode<T> | null
+  val: T;
+  next: ListNode<T> | null;
 }
 
 export function ArrToList<T>(arr: T[]): ListNode<T> | null {
-    if (!arr.length) return null
-    const node: ListNode<T> = {val: arr[0], next: null}
-    let next = node
-    for (let i = 1; i < arr.length; i++) {
-        next.next = {val: arr[i], next: null}
-        next = next.next
-    }
-    return node
+  if (!arr.length) return null;
+  const node: ListNode<T> = { val: arr[0], next: null };
+  let next = node;
+  for (let i = 1; i < arr.length; i++) {
+    next.next = { val: arr[i], next: null };
+    next = next.next;
+  }
+  return node;
 }
 
 export function addTwoNumbers(l1: ListNode<number> | null, l2: ListNode<number> | null): ListNode<number> | null {
-    let carry: number = 0
-    const Node: ListNode<number> | null = {val: 0, next: null}
-    let node = Node
+  let carry: number = 0;
+  const Node: ListNode<number> | null = { val: 0, next: null };
+  let node = Node;
 
-    while (l1 !== null || l2 !== null) {
-        let sum = (l1?.val || 0) + (l2?.val || 0) + carry
-        carry = Math.floor(sum / 10)
-        sum = sum % 10
+  while (l1 !== null || l2 !== null) {
+    let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+    carry = Math.floor(sum / 10);
+    sum = sum % 10;
 
-        node.next = {val: sum, next: null}
-        node = node.next
+    node.next = { val: sum, next: null };
+    node = node.next;
 
-        if (l1 !== null) l1 = l1.next
-        if (l2 !== null) l2 = l2.next
-    }
-    return Node.next
+    if (l1 !== null) l1 = l1.next;
+    if (l2 !== null) l2 = l2.next;
+  }
+  return Node.next;
 }
 
 export class LinkTable<T> {
-    readonly #node: T // current element
-    #next: undefined | LinkTable<T> // next node
-    #len: number = 0 // list length
+  readonly #node: T; // current element
+  #next: undefined | LinkTable<T>; // next node
+  #len: number = 0; // list length
 
-    constructor(node: T) {
-        this.#node = node
-    }
+  constructor(node: T) {
+    this.#node = node;
+  }
 
-    add = (node: T): LinkTable<T> => {
-        this.#next = new LinkTable<T>(node)
-        this.#len++
-        console.log(this.#node, this.next())
-        return this.#next
-    }
-    value = () => this.#node
-    next = () => {
-        if (this.#next) return this.value()
-    }
+  add = (node: T): LinkTable<T> => {
+    this.#next = new LinkTable<T>(node);
+    this.#len++;
+    console.log(this.#node, this.next());
+    return this.#next;
+  };
+  value = () => this.#node;
+  next = () => {
+    if (this.#next) return this.value();
+  };
 }
