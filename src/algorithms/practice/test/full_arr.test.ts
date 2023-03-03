659677848385b978f51f0b1fcd770368384d3235
Yaqf <q@x2oe.com>
2023-03-04 00:14:46 +0800

    fmt

--- src/algorithms/practice/test/full_arr.test.ts
+++ src/algorithms/practice/test/full_arr.test.ts
@@ -1,15 +1,25 @@
-import {func} from "../full_arr";
+import { func } from '../full_arr';
 
 describe('', () => {
-    it('', () => {
-        expect(func([1, 2, 3])).toEqual([[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]])
-    })
+  it('', () => {
+    expect(func([1, 2, 3])).toEqual([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]);
+  });
 
-    it('', () => {
-        expect(func([1])).toEqual([[1]])
-    })
+  it('', () => {
+    expect(func([1])).toEqual([[1]]);
+  });
 
-    it('', () => {
-        expect(func([0, 1])).toEqual([[0, 1], [1, 0]])
-    })
-})
+  it('', () => {
+    expect(func([0, 1])).toEqual([
+      [0, 1],
+      [1, 0],
+    ]);
+  });
+});
