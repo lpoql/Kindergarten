e37b5e739c0fa302b98541a5d7f1d91dbfe86410
Yaqf <q@x2oe.com>
2023-03-04 00:42:24 +0800

    fmt

--- src/algorithms/practice/full_arr.ts
+++ src/algorithms/practice/full_arr.ts
@@ -1,40 +1,40 @@
 export function permute(nums: number[]): number[][] {
-    const res: number[][] = []
-    let current: number[] = []
+  const res: number[][] = [];
+  let current: number[] = [];
 
-    function recursion(temp: number[], current: number[]) {
-        if (current.length === nums.length) return res.push([...current])
+  function recursion(temp: number[], current: number[]) {
+    if (current.length === nums.length) return res.push([...current]);
 
-        for (let i = 0; i < temp.length; i++) {
-            current.push(temp[i])
-            let t: number[] = [...temp]
-            t.splice(i, 1) // remove old
-            recursion(t, current)
-            current.pop()
-        }
+    for (let i = 0; i < temp.length; i++) {
+      current.push(temp[i]);
+      let t: number[] = [...temp];
+      t.splice(i, 1); // remove old
+      recursion(t, current);
+      current.pop();
     }
+  }
 
-    recursion(nums, current)
-    return res
-};
+  recursion(nums, current);
+  return res;
+}
 
 // [1,2,3]
 export const func = (nums: number[]): number[][] => {
-    const res: number[][] = []
+  const res: number[][] = [];
 
-    function generate(curr: number[], recorder: { [p: string]: number }) {
-        if (curr.length === nums.length) return res.push(curr.slice())
+  function generate(curr: number[], recorder: { [p: string]: number }) {
+    if (curr.length === nums.length) return res.push(curr.slice());
 
-        nums.map(each => {
-            if (recorder[each]) return
-            curr.push(each)
-            recorder[each] = 1
-            generate(curr, recorder)
-            curr.pop()
-            delete recorder[each]
-        });
-    }
+    nums.map(each => {
+      if (recorder[each]) return;
+      curr.push(each);
+      recorder[each] = 1;
+      generate(curr, recorder);
+      curr.pop();
+      delete recorder[each];
+    });
+  }
 
-    generate([], {})
-    return res
-}
+  generate([], {});
+  return res;
+};
--- src/algorithms/practice/test/full_arr.test.ts
+++ src/algorithms/practice/test/full_arr.test.ts
@@ -1,38 +1,38 @@
-import {func, permute} from '../full_arr';
+import { func, permute } from '../full_arr';
 
 describe('', () => {
-    it('', () => {
-        expect(func([1, 2, 3])).toEqual([
-            [1, 2, 3],
-            [1, 3, 2],
-            [2, 1, 3],
-            [2, 3, 1],
-            [3, 1, 2],
-            [3, 2, 1],
-        ]);
-        expect(permute([1, 2, 3])).toEqual([
-            [1, 2, 3],
-            [1, 3, 2],
-            [2, 1, 3],
-            [2, 3, 1],
-            [3, 1, 2],
-            [3, 2, 1],
-        ])
-    });
+  it('', () => {
+    expect(func([1, 2, 3])).toEqual([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]);
+    expect(permute([1, 2, 3])).toEqual([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]);
+  });
 
-    it('', () => {
-        expect(func([1])).toEqual([[1]]);
-        expect(permute([1])).toEqual([[1]]);
-    });
+  it('', () => {
+    expect(func([1])).toEqual([[1]]);
+    expect(permute([1])).toEqual([[1]]);
+  });
 
-    it('', () => {
-        expect(func([0, 1])).toEqual([
-            [0, 1],
-            [1, 0],
-        ]);
-        expect(permute([0, 1])).toEqual([
-            [0, 1],
-            [1, 0],
-        ]);
-    });
+  it('', () => {
+    expect(func([0, 1])).toEqual([
+      [0, 1],
+      [1, 0],
+    ]);
+    expect(permute([0, 1])).toEqual([
+      [0, 1],
+      [1, 0],
+    ]);
+  });
 });
